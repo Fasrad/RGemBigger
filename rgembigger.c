1fdb4926411eed0d548f75840e005c37e7405579
Fasrad <millerchaz@gmail.com>
2014-07-26 23:39:52 -0400

    updated timer setups

--- rgembigger.c
+++ rgembigger.c
@@ -24,16 +24,17 @@ void blink (uint8_t);
 int main(){
     //set up PWMS for LED dimmage
     //8-bit Timer 0 used for green PWM 
-        TCCR0B = (1<<CS22)|(1<<CS21)|(1<<CS20);                //CPU/1024; 2Hz
+        TCCR0B = (1<<CS01);                  //timer clock is CPU/8 (15kHz pwm)
+        TCCR0B = (1<<WGM01)|(1<<WGM00);      //Fast pwm mode p.108
+        TCCR0B = (1<<COM0B1);                //clear on compare match
     //16-bit Timer 1 used as blue PWM on OC1B PB2 (Arduino pin 10) p.115
-    //noninverting phase correct, CTC-PWM hybrid mode p135 
-        TCCR1A = (1<<COM1B1)|(1<<WGM11)|(1<<WGM10); 
-        OCR1A = 0x0FF0;             //sets pwm TOP 
-        OCR1B = 0;
-        TCCR1B = (1<<WGM13)|(1<<CS10);                 //  clk/1; 240kHz
-        CLKPR = 0x80; CLKPR = 0;                       //  clk*8; 2kHz
+    //noninverting fast PWM mode p135 
+        TCCR1B = (1<<CS11)|(1<<WGM12);                    // clk/8; fast pwm
+        TCCR1A = (1<<COM1B1)|(1<<WGM10);                  //TOP=0xFF; fast pwm
     //8 bit Timer 2 for red PWM 
-        TCCR2B = (1<<CS22)|(1<<CS21)|(1<<CS20);                //CPU/1024; 2Hz
+        TCCR2B = (1<<CS21);                  //timer clock is CPU/8 (15kHz pwm)
+        TCCR2B = (1<<WGM21)|(1<<WGM20);      //Fast pwm mode p.160
+        TCCR2B = (1<<COM2B1);                //clear on compare match
     PORTB = 0xFF;
     DDRB = 0b000100100;              //LED on PB5; OC1B is PB2
     PORTB|=1<<5;                     //turn on LED
